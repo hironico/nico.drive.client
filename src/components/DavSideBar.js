be6520ed79c03f609355e5f0877f340341584cba
hironico <hironico@github.com>
2023-03-29 08:56:02 +0200

    more space for dir tree and colors aligned

--- src/components/DavSideBar.js
+++ src/components/DavSideBar.js
@@ -18,15 +18,15 @@ class DavSideBar extends Component {
     }
 
     render = () => {
-        return <Pane className="davsidebar" gridTemplateRows="auto auto auto 1fr" gridTemplateColumns="auto" height="100vh" minWidth={170} padding={15} overflow="hidden" background="blueTint" elevation={0} >
-            <Pane background="blueTint">
+        return <Pane className="davsidebar" gridTemplateRows="auto auto 1fr" gridTemplateColumns="auto" height="100vh" minWidth={170} overflow="hidden" background="tint2" elevation={0} >
+            <Pane marginTop={15} marginLeft={10} marginRight={10} >
                 <Heading size={900} color="neutral" textAlign="left">Nico's drive</Heading>
             </Pane>
-            <Pane background="blueTint" marginTop={15}>
+            <Pane marginTop={15} marginLeft={10} marginRight={10}>
                 <Heading size={600} color="neutral" textAlign="left">My drives:</Heading>
-            </Pane>
-            <DavRootSelector handleNavigate={this.props.handleNavigate} width="100%" />
-            <Pane display="grid" gridTemplateColumns="1fr" width="100%" overflow="hidden" marginTop={15}>
+                <DavRootSelector handleNavigate={this.props.handleNavigate} width="100%" />
+            </Pane>            
+            <Pane display="grid" gridTemplateColumns="1fr" width="100%" overflow="hidden" marginTop={15} >
                 <Tree key={this.context.selectedUserRootDirectory.name} rootDirs={this.props.rootDirs} handleNavigate={this.props.handleNavigate} currentDirectory={this.props.currentDirectory} />    
             </Pane>            
         </Pane>
