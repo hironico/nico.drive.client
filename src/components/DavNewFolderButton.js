45ff4942a3bfb4fd09dc1e660d8f5b69ffabe81d
hironico <hironico@github.com>
2023-03-30 11:05:32 +0200

    displays a toast when create folder fails

--- src/components/DavNewFolderButton.js
+++ src/components/DavNewFolderButton.js
@@ -1,50 +1,60 @@
 
 import { Component } from 'react';
-import { Popover, Pane, Text, TextInput, Button, FolderNewIcon, Checkbox, IconButton } from 'evergreen-ui';
+import { Popover, Pane, Text, TextInput, Button, FolderNewIcon, Checkbox, IconButton, toaster } from 'evergreen-ui';
 import { DavConfigurationContext } from '../AppSettings';
 
 export default class DavNewFolderButton extends Component {
-    static contextType = DavConfigurationContext;
+  static contextType = DavConfigurationContext;
 
-    constructor() {
-        super();
-        this.state = {
-            newFolderName: '',
-            moveToFolder: true
-        }
+  constructor() {
+    super();
+    this.state = {
+      newFolderName: '',
+      moveToFolder: true
+    }
+  }
+
+  createFolder = async () => {
+    const separator = this.props.currentDirectory.endsWith('/') ? '' : '/';    
+    const newDir = `${this.props.currentDirectory}${separator}${this.state.newFolderName}`;
+    let success = true;
+    try {
+      await this.context.selectedUserRootDirectory.davClient.createDirectory(newDir);
+    } catch (error) {
+      toaster.danger(`Cannot create ${newDir}. ${error.message}`);
+      success = false;
     }
 
-    createFolder = async () => {
-        const newDir = `${this.props.currentDirectory}/${this.state.newFolderName}`;
-        await this.context.selectedUserRootDirectory.davClient.createDirectory(newDir);
-        this.setState({
-            newFolderName: '',
-            popoverShown: false
-        }, () => {
-            const destDir = this.state.moveToFolder ? newDir : this.props.currentDirectory;
-            this.props.handleNavigate(destDir);
-        });
-    }
+    this.setState({
+      newFolderName: '',
+      popoverShown: false
+    }, () => {
+      if (success) {
+        const destDir = this.state.moveToFolder ? newDir : this.props.currentDirectory;
+        this.props.handleNavigate(destDir);
+      }
+    });
+  }
 
-    render = () => {
-        return <Popover
-        content={({ close }) => {
-          return <Pane
-            padding={20}
-            display="grid"
-            alignItems="left"
-            justifyContent="left"
-            gridTemplateColumns="auto auto"
-            gridTemplateRows="auto auto auto"
-          >
-            <Text gridColumnStart="1" gridColumnEnd="span 2">New folder name:</Text>
-            <TextInput gridColumnStart="1" gridColumnEnd="span 2" width="100%" marginTop={5} value={this.state.newFolderName} onChange={(evt) => this.setState({newFolderName: evt.target.value})} />
-            <Checkbox gridColumnStart="1" gridColumnEnd="span 2" width="100%" marginTop={5} disabled={this.state.newFolderName === ''} label="Move to new folder afer create" checked={this.state.moveToFolder} onChange={(evt) => this.setState({moveToFolder: !this.state.moveToFolder})} />
-            <Button is='div' marginTop={10} marginRight={5} alignSelf="left" appearance='primary' intent='info' disabled={this.state.newFolderName === ''} onClick={evt => this.createFolder()}>Create</Button>
-            <Button is="div" marginTop={10} marginLeft={5} alignSelf="right" appearance='default' intent='none' onClick={close}>Close</Button>
-          </Pane>
-        }}>
-        <IconButton is="div" marginLeft={6} marginRight={6} icon={FolderNewIcon} appearance="primary" intent="info" />
-      </Popover>
-    }
+  render = () => {
+    return <Popover
+      content={({ close }) => {
+        return <Pane
+          padding={20}
+          display="grid"
+          alignItems="left"
+          justifyContent="left"
+          gridTemplateColumns="auto auto"
+          gridTemplateRows="auto auto auto"
+        >
+          <Text gridColumnStart="1" gridColumnEnd="span 2">New folder name:</Text>
+          <TextInput gridColumnStart="1" gridColumnEnd="span 2" width="100%" marginTop={5} value={this.state.newFolderName} onChange={(evt) => this.setState({ newFolderName: evt.target.value })} />
+          <Checkbox gridColumnStart="1" gridColumnEnd="span 2" width="100%" marginTop={5} disabled={this.state.newFolderName === ''} label="Move to new folder afer create" checked={this.state.moveToFolder} onChange={(evt) => this.setState({ moveToFolder: !this.state.moveToFolder })} />
+          <Button is='div' marginTop={10} marginRight={5} alignSelf="left" appearance='primary' intent='info' disabled={this.state.newFolderName === ''} onClick={evt => this.createFolder()}>Create</Button>
+          <Button is="div" marginTop={10} marginLeft={5} alignSelf="right" appearance='default' intent='none' onClick={close}>Close</Button>
+        </Pane>
+      }}>
+      <IconButton is="div" marginLeft={6} marginRight={6} icon={FolderNewIcon} appearance="primary" intent="info" />
+    </Popover>
+  }
 }
