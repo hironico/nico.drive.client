8b053711f4ed900f4c64feda2056b37d67f9c672
hironico <hironico.pub@outlook.com>
2025-04-21 08:56:43 +0200

    Hot code replacement now works with vite. Moved to JSX !

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig, transformWithEsbuild } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      async transform(code, id) {
-        if (!id.match(/src\/.*\.js$/))  return null
-
-        // Use the exposed transform from vite, instead of directly
-        // transforming with esbuild
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic',
-        })
-      },
-    },
+  plugins: [    
     react(),
   ],
 
